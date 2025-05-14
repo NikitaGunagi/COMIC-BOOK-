1334e9992467d4b6c41f62fdbe5da82f6638354c
NikitaGunagi <161939863+NikitaGunagi@users.noreply.github.com>
2025-05-14 21:33:04 +0530

    Add files via upload

---
+++ client/src/Component/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-dark">
+      <div className="container-fluid container d-flex align-items-center">
+        <Link to="/" className="text-decoration-none">
+          <p className="nav-brand fw-bold fs-3 text-white">ComicShelf</p>
+        </Link>
+        <div className="d-flex align-items-center">
+          <Link to="/" className="text-decoration-none text-white fw-bold btn fs-5 mb-0 me-3">
+            Home
+          </Link>
+          <Link to="/new-book">
+            <button type="button" className="btn btn-primary fs-5">Add a New Book</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
